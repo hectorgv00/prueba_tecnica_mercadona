51ad0925d316604fd4afd6f55b2de3a52575a498
Hector Gonzalez Viejo <hector.gv00@gmail.com>
2025-02-19 12:54:17 +0100

    docs(loginService): Documentado el servicio

--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -2,6 +2,10 @@ import { BehaviorSubject } from 'rxjs';
 import { iLoginInformation } from '../interfaces/iLoginInformation';
 
 export class LoginService {
+  // We create a BehaviorSubject to store the login information, the information is an object with the following properties:
+  // hasToken: boolean, it indicates if the user has a token
+  // username: string, it is the username of the user
+  // hasTokenAndUsername: boolean, it indicates if the user has a token and a username, so it is logged in
   private _loginInformation: BehaviorSubject<iLoginInformation> =
     new BehaviorSubject<iLoginInformation>({
       hasToken: false,
@@ -9,10 +13,12 @@ export class LoginService {
       hasTokenAndUsername: false,
     });
 
+  // We create an observable to subscribe to the behavior subject
   get loginInformation() {
     return this._loginInformation.asObservable();
   }
 
+  // We simulate the login process, setting the username provided and all the rest of the information as if the user is logged in
   private login(username: string) {
     localStorage.setItem('mercadona_token', 'tenemos_token');
     localStorage.setItem('mercadona_username', username);
@@ -23,6 +29,7 @@ export class LoginService {
     });
   }
 
+  // We simulate the logout process, removing the token and username from the local storage and updating the behavior subject
   public logout() {
     localStorage.removeItem('mercadona_token');
     localStorage.removeItem('mercadona_username');
@@ -33,6 +40,7 @@ export class LoginService {
     });
   }
 
+  // Function that handles the login process, if the user has a token and a username, we log in, otherwise we log out
   public handleLogin(loginInformation: iLoginInformation) {
     if (loginInformation.hasTokenAndUsername) {
       this.login(loginInformation.username);
