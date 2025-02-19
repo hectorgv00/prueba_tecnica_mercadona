45e65242a489aa0b39bb7d4f3e567de087d352c0
Hector Gonzalez Viejo <hector.gv00@gmail.com>
2025-02-19 10:59:28 +0100

    feat(header): añadida la imagen corporativa con el nombre en el header

---
+++ src/app/layout/complete-layout/complete-layout.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { NgOptimizedImage } from '@angular/common';
+
+@Component({
+  selector: 'app-complete-layout',
+  imports: [NgOptimizedImage],
+  templateUrl: './complete-layout.component.html',
+  styleUrl: './complete-layout.component.scss',
+})
+export class CompleteLayoutComponent {}
