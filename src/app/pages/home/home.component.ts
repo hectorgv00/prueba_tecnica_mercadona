7f93da9fbf85404a7f81bba093bd72dce40bb770
Hector Gonzalez Viejo <hector.gv00@gmail.com>
2025-02-19 17:42:27 +0100

    feat(homeComponent): Creada la tarjeta de tornillos

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
+import { ButtonComponent } from '../../components/button/button.component';
+import { iButtonOptions } from '../../interfaces/iButtonOptions';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [ButtonComponent],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
 export class HomeComponent {
-
+  revisarButtonOptions: iButtonOptions = {
+    class: 'primary',
+    text: 'Revisar',
+    disabled: false,
+    onClick: () => {
+      console.log('Revisar button clicked');
+    },
+    icon: 'visibility',
+  };
 }
