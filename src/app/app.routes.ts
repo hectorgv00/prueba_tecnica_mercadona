7eb33f9e2678c5e05c007150e5fd5d23cf29e699
Hector Gonzalez Viejo <hector.gv00@gmail.com>
2025-02-19 18:39:39 +0100

    feat(routes): Añadida la ruta /tornillos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,16 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { TornillosPageComponent } from './pages/tornillos-page/tornillos-page.component';
+import { authGuard } from './guards/auth-guard/auth.guard';
 
 export const routes: Routes = [
   {
     path: '',
     component: HomeComponent,
+  },
+  {
+    path: 'tornillos',
+    component: TornillosPageComponent,
+    canActivate: [authGuard],
   },
 ];
