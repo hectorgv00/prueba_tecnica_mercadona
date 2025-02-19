3330339b451c1cc312356206726e1e8c0f26e754
Hector Gonzalez Viejo <hector.gv00@gmail.com>
2025-02-19 10:14:35 +0100

    init: initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CompleteLayoutComponent } from './layout/complete-layout/complete-layout.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CompleteLayoutComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'prueba_tecnica_mercadona';
