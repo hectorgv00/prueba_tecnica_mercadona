5fae8b78230cd0a24c9cb4f299f4b4e7ffa3b31a
Hector Gonzalez Viejo <hector.gv00@gmail.com>
2025-02-20 21:03:13 +0100

    test(buttonComponent): Añadidos tests para el button component

--- src/app/components/button/button.component.spec.ts
+++ src/app/components/button/button.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ButtonComponent } from './button.component';
+import { By } from '@angular/platform-browser';
 
 describe('ButtonComponent', () => {
   let component: ButtonComponent;
@@ -8,16 +9,48 @@ describe('ButtonComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ButtonComponent]
-    })
-    .compileComponents();
+      imports: [ButtonComponent],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(ButtonComponent);
     component = fixture.componentInstance;
+
+    component.buttonOptions = {
+      text: 'Click me',
+      onClick: () => {},
+      class: 'primary',
+      disabled: false,
+    };
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should render the button', () => {
+    const buttonElement = fixture.debugElement.query(By.css('button'));
+    expect(buttonElement).toBeTruthy();
+  });
+
+  it('should call onClick when button is clicked', () => {
+    spyOn(component.buttonOptions, 'onClick');
+    const buttonElement = fixture.debugElement.query(By.css('button'));
+    buttonElement.triggerEventHandler('click', null);
+    expect(component.buttonOptions.onClick).toHaveBeenCalled();
+  });
+
+  it('should have the correct class', () => {
+    component.buttonOptions.class = 'primary';
+    fixture.detectChanges();
+    const buttonElement = fixture.debugElement.query(By.css('button'));
+    expect(buttonElement.nativeElement.classList).toContain('primary');
+  });
+
+  it('should be disabled when disabled is true', () => {
+    component.buttonOptions.disabled = true;
+    fixture.detectChanges();
+    const buttonElement = fixture.debugElement.query(By.css('button'));
+    expect(buttonElement.nativeElement.disabled).toBeTrue();
   });
 });
