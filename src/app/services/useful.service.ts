5c15fe76011c3e675dd10f39f752dadc4034d063
Hector Gonzalez Viejo <hector.gv00@gmail.com>
2025-02-20 17:51:37 +0100

    fix(swapColumnsFunction): No estaba funcionando bien cuando intentabas intercambiar un elemento a una posición que estaba pineada. Solucionado

--- src/app/services/useful.service.ts
+++ src/app/services/useful.service.ts
@@ -5,6 +5,15 @@ import { iTableHeaderAndVariable } from '../interfaces/iTableHeaderAndVariable';
   providedIn: 'root',
 })
 export class UsefulService {
+  // This method is used to reset the indexes of an array of objects
+  static resetIndexes(array: any[]) {
+    return array.map((el, index) => {
+      el.index = index;
+      return el;
+    });
+  }
+
+  // This method is used to swap the elements of an array of objects
   static swapArrayElements(
     arrayToSort: iTableHeaderAndVariable[],
     indexA: number,
@@ -15,51 +24,34 @@ export class UsefulService {
     const nonPinnedElements: iTableHeaderAndVariable[] = [];
     const availableIndices: number[] = [];
 
-    arrayToSort.forEach((element: iTableHeaderAndVariable, index: number) => {
-      // If the elment is pinned, it is sent to the pinnedElements array
-      if (element.pinned) {
-        pinnedElements.push({ index, element });
-      } else {
-        // Otherwise, it is sent to the nonPinnedElements array and the index is added to the availableIndices array
-        nonPinnedElements.push(element);
-        availableIndices.push(index);
-      }
-    });
-
-    // Find the sourceIndex in nonPinnedElements
-    const elementToMove = arrayToSort[indexA];
-    const sourceIndex = nonPinnedElements.findIndex(
-      (el) => el === elementToMove
+    // We get the splitted arrays
+    this.separetePinnedAndNonPinnedElements(
+      arrayToSort,
+      pinnedElements,
+      nonPinnedElements,
+      availableIndices
     );
 
-    // If for some reason the element is not found, return the original array. This should not happen, as when an element is pinned, it is not draggable
-    if (sourceIndex === -1) {
-      return arrayToSort;
-    }
-
-    // Now we check if the target index is in the availableIndices array
-    let targetIndexInNonPinned: number;
-    const indexBInAvailable = availableIndices.indexOf(indexB);
-    if (indexBInAvailable !== -1) {
-      // If it is, we use that index
-      targetIndexInNonPinned = indexBInAvailable;
-    } else {
-      // If it is not, we find the index where it should be inserted, by finding the first index greater than indexB so we can insert it there
-      let insertionPoint = availableIndices.findIndex((ai) => ai > indexB);
-      // If the index is not found, we insert it at the end of the array
-      targetIndexInNonPinned =
-        insertionPoint === -1 ? availableIndices.length : insertionPoint;
-    }
-
-    // We make sure the target index is within the bounds of the nonPinnedElements array
-    targetIndexInNonPinned = Math.max(
-      0,
-      Math.min(targetIndexInNonPinned, nonPinnedElements.length)
+    // We get the sourceIndex in the nonPinnedElements array
+    const sourceIndex = this.getSourceIndex(
+      arrayToSort,
+      indexA,
+      nonPinnedElements
     );
 
-    // Reorder the non-pinned elements
+    // We get the targetIndex in the nonPinnedElements array
+    const targetIndexInNonPinned = this.getTargetIndexInNonPinned(
+      availableIndices,
+      indexB
+    );
+
+    // Create a new array with the non-pinned elements sorted
     const newNonPinned = [...nonPinnedElements];
-    const [movedElement] = newNonPinned.splice(sourceIndex, 1);
+
+    // We get the element that we are going to move from the sourceIndex in the newNonPinned array
+    const movedElement = newNonPinned.splice(sourceIndex, 1)[0];
+
+    // We insert the element in the targetIndexInNonPinned in the newNonPinned array
     newNonPinned.splice(targetIndexInNonPinned, 0, movedElement);
 
     // Create a new array with the same length as the original array
@@ -70,22 +62,82 @@ export class UsefulService {
       newArray[index] = element;
     });
 
-    // Fill the non-pinned elements in their respective indices
-    let nonPinnedIndex = 0;
-
     // We use the availableIndices array to know where to insert the non-pinned elements in the newArray
-    availableIndices.forEach((index) => {
-      newArray[index] = newNonPinned[nonPinnedIndex++];
+    availableIndices.forEach((index: number, i: number) => {
+      newArray[index] = newNonPinned[i];
     });
 
     return newArray;
   }
 
-  static resetIndexes(array: any[]) {
-    return array.map((el, index) => {
-      el.index = index;
-      return el;
+  // This method is used to separate the pinned elements from the non-pinned elements
+  private static separetePinnedAndNonPinnedElements(
+    arrayToSort: iTableHeaderAndVariable[],
+    pinnedElements: tPinedElements[],
+    nonPinnedElements: iTableHeaderAndVariable[],
+    availableIndices: number[]
+  ) {
+    arrayToSort.forEach((element: iTableHeaderAndVariable, index: number) => {
+      // If the elment is pinned, it is sent to the pinnedElements array
+      if (element.pinned) {
+        pinnedElements.push({ index, element });
+      } else {
+        // Otherwise, it is sent to the nonPinnedElements array and the index is added to the availableIndices array
+        nonPinnedElements.push(element);
+        availableIndices.push(index);
+      }
     });
+  }
+
+  // This method is used to get the sourceIndex in the nonPinnedElements array
+  private static getSourceIndex(
+    arrayToSort: iTableHeaderAndVariable[],
+    indexA: number,
+    nonPinnedElements: iTableHeaderAndVariable[]
+  ) {
+    // Find the sourceIndex in nonPinnedElements
+    const elementToMove = arrayToSort[indexA];
+    const sourceIndex = nonPinnedElements.findIndex(
+      (el) => el === elementToMove
+    );
+    return sourceIndex;
+  }
+
+  // This method is used to get the targetIndex in the nonPinnedElements array
+  private static getTargetIndexInNonPinned(
+    availableIndices: number[],
+    indexB: number
+  ) {
+    let targetIndexInNonPinned: number;
+
+    // We search for the indexB in the availableIndices array
+    const indexBInAvailable = availableIndices.indexOf(indexB);
+
+    // If it is found, we use it as the target index
+    if (indexBInAvailable !== -1) {
+      targetIndexInNonPinned = indexBInAvailable;
+      return targetIndexInNonPinned;
+    }
+    // If it is not found, we look for the indexes that are before indexB
+    const indexesBeforeIndexB = availableIndices.filter(
+      (availableIndex) => availableIndex < indexB
+    );
+
+    // If there are indexes before indexB
+    if (indexesBeforeIndexB.length > 0) {
+      // We get the last index before B
+      const lastÏndexBEforeB =
+        indexesBeforeIndexB[indexesBeforeIndexB.length - 1];
+
+      // And we set it as the target index
+      targetIndexInNonPinned = availableIndices.indexOf(lastÏndexBEforeB);
+    } else {
+      // If there are no indexes before indexB, we set the target index to 0
+      targetIndexInNonPinned = 0;
+    }
+
+    // we return the target index
+    return targetIndexInNonPinned;
   }
 }
 
