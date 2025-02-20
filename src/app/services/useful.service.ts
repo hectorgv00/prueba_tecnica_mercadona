b2524221474a80a07a18405ba51d10684271aab4
Hector Gonzalez Viejo <hector.gv00@gmail.com>
2025-02-20 12:15:25 +0100

    feat(usefulService): Creado el servicio para funciones útiles en la aplicación

---
+++ src/app/services/useful.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UsefulService {
+  static swapArrayElements(array: any[], indexA: number, indexB: number) {
+    const element = array[indexA];
+
+    const befroeElementArray = array
+      .slice(0, indexA)
+      .filter((el) => el !== element);
+    const afterElementArray = array
+      .slice(indexA + 1)
+      .filter((el) => el !== element);
+
+    const beforeElementArrayLength = befroeElementArray.length;
+
+    if (indexB > beforeElementArrayLength) {
+      const afterIndexB = indexB - beforeElementArrayLength;
+      return [
+        ...befroeElementArray,
+        ...afterElementArray.slice(0, afterIndexB),
+        element,
+        ...afterElementArray.slice(afterIndexB),
+      ];
+    } else {
+      return [
+        ...befroeElementArray.slice(0, indexB),
+        element,
+        ...befroeElementArray.slice(indexB),
+        ...afterElementArray,
+      ];
+    }
+  }
+}
