93e1b8756960e6fa5a991e5e584d6ab4a57e61d5
Hector Gonzalez Viejo <hector.gv00@gmail.com>
2025-02-20 18:50:53 +0100

    feat(newTornilloModal): Establecida la lógica y el estilo del new tornillo modal

--- src/app/modal/new-tornillo-modal/new-tornillo-modal.component.ts
+++ src/app/modal/new-tornillo-modal/new-tornillo-modal.component.ts
@@ -15,6 +15,9 @@ import { MatInputModule } from '@angular/material/input';
 import { ButtonComponent } from '../../components/button/button.component';
 import { iButtonOptions } from '../../interfaces/iButtonOptions';
 import { MatIconModule } from '@angular/material/icon';
+import { MatSelectModule } from '@angular/material/select';
+import { Subscription } from 'rxjs';
+import { TornillosService } from '../../services/tornillos.service';
 
 @Component({
   selector: 'app-new-tornillo-modal',
@@ -25,13 +28,20 @@ import { MatIconModule } from '@angular/material/icon';
     MatInputModule,
     ButtonComponent,
     MatIconModule,
+    MatSelectModule,
   ],
   templateUrl: './new-tornillo-modal.component.html',
   styleUrl: './new-tornillo-modal.component.scss',
 })
 export class NewTornilloModalComponent {
   newTornilloForm: FormGroup = new FormGroup({
-    username: new FormControl('', Validators.required),
+    nombre: new FormControl('', [
+      Validators.required,
+      Validators.maxLength(50),
+    ]),
+    precio: new FormControl(0, [Validators.required, Validators.min(0)]),
+    formato: new FormControl('', Validators.required),
+    marca: new FormControl('', Validators.required),
   });
 
   cancelButtonOptions: iButtonOptions = {
@@ -48,10 +58,21 @@ export class NewTornilloModalComponent {
     onClick: () => this.saveForm(),
   };
 
+  // subscribe
+  newTornilloFormSubscription: Subscription | null = null;
+
+  formats: string[] = [];
+
   constructor(
     @Inject(MAT_DIALOG_DATA) public data: any,
-    private dialogRef: MatDialogRef<NewTornilloModalComponent>
+    private dialogRef: MatDialogRef<NewTornilloModalComponent>,
+    private tornillosSE: TornillosService
   ) {}
+
+  ngOnInit() {
+    this.suscribeToForm();
+    this.getFormats();
+  }
 
   closePopup(): void {
     this.dialogRef.close(false);
@@ -59,5 +80,42 @@ export class NewTornilloModalComponent {
 
   saveForm(): void {
     this.dialogRef.close(this.newTornilloForm.value);
+  }
+
+  removePrice(): void {
+    const currentValue = this.newTornilloForm.controls['precio'].value;
+    const valueToSet = Math.round((currentValue - 0.1) * 10) / 10;
+
+    if (currentValue <= 0) {
+      this.newTornilloForm.controls['precio'].setValue(0);
+      return;
+    }
+
+    this.newTornilloForm.controls['precio'].setValue(valueToSet);
+  }
+
+  addPrice(): void {
+    const currentValue = this.newTornilloForm.controls['precio'].value;
+    const valueToSet = Math.round((currentValue + 0.1) * 10) / 10;
+    console.log(valueToSet);
+    this.newTornilloForm.controls['precio'].setValue(valueToSet);
+  }
+
+  suscribeToForm() {
+    this.newTornilloFormSubscription =
+      this.newTornilloForm.statusChanges.subscribe((status) => {
+        this.saveButtonOptions.disabled = status !== 'VALID';
+      });
+  }
+
+  checkPrice(event: Event): void {
+    const value = this.newTornilloForm.controls['precio'].value;
+    if (value < 0) {
+      this.newTornilloForm.controls['precio'].setValue(0);
+    }
+  }
+
+  getFormats(): void {
+    this.formats = this.tornillosSE.getFormatosUnique();
   }
 }
