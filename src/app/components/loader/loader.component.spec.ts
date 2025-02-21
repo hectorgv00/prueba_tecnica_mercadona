2262274d088abb6a8a45096654dcde16271a03ca
Hector Gonzalez Viejo <hector.gv00@gmail.com>
2025-02-21 09:22:36 +0100

    test(loaderComponent): Añadido el test de si se renderiza la imagen del loader

--- src/app/components/loader/loader.component.spec.ts
+++ src/app/components/loader/loader.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LoaderComponent } from './loader.component';
+import { By } from '@angular/platform-browser';
 
 describe('LoaderComponent', () => {
   let component: LoaderComponent;
@@ -8,9 +9,8 @@ describe('LoaderComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [LoaderComponent]
-    })
-    .compileComponents();
+      imports: [LoaderComponent],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(LoaderComponent);
     component = fixture.componentInstance;
@@ -19,5 +19,11 @@ describe('LoaderComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should render the loader image', () => {
+    const image = fixture.debugElement.query(By.css('img'));
+    expect(image).toBeTruthy();
+    expect(image.nativeElement.src).toContain('/images/loader.svg');
   });
 });
